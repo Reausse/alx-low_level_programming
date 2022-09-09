b98559e6fd683584001e8a269d62de204ceaf48a
Reausse <mailto:amprosemond@gmail.com>
2022-09-08 21:49:53 -0700

    Print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+
+/**
+*main - print the string in the function
+*
+*Description: using the main function
+*this program prints "Programming is like building a multilingual puzzle
+*Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a pieceof art,\n");
+	return (0);
+}
